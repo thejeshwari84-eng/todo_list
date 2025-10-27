46234af5e6d36d67ccef8ddd8320ae4cc922ccb3
thejeshwari84-eng <thejeshwari84@gmail.com>
2025-10-27 19:54:18 +0530

    Updated To-Do List app with new features and UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,85 +3,137 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(mainApp());
 }
-class mainApp extends StatefulWidget {
 
+class mainApp extends StatefulWidget {
   @override
   State<mainApp> createState() => _mainAppState();
 }
 
 class _mainAppState extends State<mainApp> {
-  String DisplayText = "No Text Data";
-  List<String>taskList = [];
-  TextEditingController textController= TextEditingController();
+  List<String> taskList = [];
+  TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Todo List App"),
           backgroundColor: Colors.blue,
-          centerTitle: false,
+          centerTitle: true,
         ),
         body: Column(
           children: [
+            // Add Task Row
             Row(
               children: [
-            Expanded(child: Container(
-              padding: EdgeInsets.all(10),
-              child: 
-                  TextField(
-                    controller: textController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                label: Text("Enter a Task")
-              ),
-            ) 
-            ),),
-            MaterialButton(
-              color: Colors.white,
-              height: 50,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              onPressed: () {
-                setState(() {
-                 
-
-                  taskList.add(textController.text);
-                  textController.clear();
-                });
-              }, child: Text("Add"),),
-              ],
-            ),
-             
-            Flexible(
-              child: ListView.builder(
-                itemCount: taskList.length,
-                itemBuilder: (context, index){
-                return Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      child:Text(taskList[index]) ,
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      controller: textController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Enter a Task",
+                      ),
                     ),
                   ),
-                
-                  MaterialButton(
-                  
-                    child: Icon(Icons.delete, color: Colors.red, ),
-                    onPressed: () {
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  height: 50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  onPressed: () {
+                    if (textController.text.isNotEmpty) {
                       setState(() {
-                        taskList.removeAt(index);
+                        taskList.add(textController.text);
+                        textController.clear();
                       });
                     }
-                    )
-                ],
-              );
-              }),
+                  },
+                  child: Text(
+                    "Add",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
             ),
-              
+
+            // Task List
+            Expanded(
+              child: ListView.builder(
+                itemCount: taskList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: ListTile(
+                      title: Text(taskList[index]),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // 🗑 Delete Button
+                          IconButton(
+                            icon: Icon(Icons.delete, color: Colors.red),
+                            onPressed: () {
+                              setState(() {
+                                taskList.removeAt(index);
+                              });
+                            },
+                          ),
+
+                          // ✏ Edit Button
+                          IconButton(
+                            icon: Icon(Icons.edit, color: Colors.blue),
+                            onPressed: () {
+                              TextEditingController editController =
+                                  TextEditingController(
+                                      text: taskList[index]);
+
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text("Edit Task"),
+                                    content: TextField(
+                                      controller: editController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        labelText: "Edit your task",
+                                      ),
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text("Cancel"),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            taskList[index] =
+                                                editController.text;
+                                          });
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text("Save"),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,8 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// ignore_for_file: duplicate_import
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_application_3/main.dart';
 
 import 'package:flutter_application_3/main.dart';
 
